0bf06a6547526491c617e1b9d494ccb61409ca2e
OthmanHaba <alvondrvio@gmail.com>
2023-01-31 21:53:03 +0200

    change the title

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Flutter s;jgns;gjs;gjnw;gjweApp'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
